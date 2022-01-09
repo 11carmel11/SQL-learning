08ef3825f6aee9f78da2c1b31b20e89348d33655
11carmel11 <11carmrl11@gmail.com>
2022-01-09 18:48:56 +0200

    add pupil endpoint

--- 38.sql-erd/routers/pupils.js
+++ 38.sql-erd/routers/pupils.js
@@ -10,4 +10,35 @@ router.get("/:pupilId", (req, res) => {
   });
 });
 
+router.post("/new/pupil", (req, res) => {
+  const { name, class: classNum, subjects } = req.body;
+  let id;
+  const sql = `INSERT INTO pupils(name, classes_id) VALUES (${name}, ${classNum})`;
+  con.query(sql, (err) => {
+    if (err) throw err;
+  });
+
+  con.query("SELECT * FROM pupils ORDER BY id DESC", (err, results) => {
+    if (err) throw err;
+    id = results[0].id;
+  });
+
+  for (const subject of subjects) {
+    const subjQuery = `SELECT * FROM subjects WHERE subject = ${subject}`;
+    let subjId;
+    con.query(subjQuery, (err, results) => {
+      if (err) throw err;
+      subjId = results[0].id;
+    });
+    con.query(
+      `INSERT INTO pupils_has_subjects VALUES (${id}, ${subjId})`,
+      (err) => {
+        if (err) throw err;
+      }
+    );
+  }
+
+  res.sendStatus(201);
+});
+
 module.exports = router;
