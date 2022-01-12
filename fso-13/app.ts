b463ab9cbb6f06dd61a06c3b1a2197d7b2ac26d3
11carmel11 <11carmrl11@gmail.com>
2022-01-12 15:59:54 +0200

    3.5+3.6-adding update endpoint

--- fso-13/app.ts
+++ fso-13/app.ts
@@ -1,35 +1,12 @@
 require("dotenv").config();
-import express, { Request } from "express";
-import Blogs from "./model/blogs";
-import { Blog } from "./types";
+import express from "express";
+import blogsRouter from "./routers/blogs";
 
 const app = express();
 
 app.use(express.json());
-
-app.get("/api/blogs", async (_req: Request<never, Blog[] | []>, res) => {
-  const blogs = (await Blogs.findAll()).map((blog) => blog.toJSON());
-  res.json(blogs);
-});
-
-app.post("/api/blogs", async (req: Request<never, Blog | void, Blog>, res) => {
-  const dataFromClient: Blog = req.body;
-  const newBlog: Blog = (await Blogs.create(dataFromClient)).toJSON();
-  res.json(newBlog);
-});
-
-app.get(
-  "/api/blogs/:id",
-  async (req: Request<{ id: string }, Blog | string, never>, res) => {
-    const { id } = req.params;
-    const blog = await Blogs.findByPk(Number(id));
-    if (blog) {
-      res.json(blog.toJSON());
-    } else {
-      res.sendStatus(404);
-    }
-  }
-);
+app.use(express.urlencoded({ extended: false }));
+app.use("/api/blogs", blogsRouter);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
---
+++ fso-13/routers/blogs.ts
@@ -0,0 +1,49 @@
+import { Request, Router } from "express";
+import Blogs from "../model/blogs";
+import { Blog } from "../types";
+
+const router = Router(); // /api/blogs router
+
+router.get("/", async (_req: Request<never, Blog[] | []>, res) => {
+  const blogs: Blog[] = (await Blogs.findAll()).map((blog) => blog.toJSON());
+  res.json(blogs);
+});
+
+router.post("/", async (req: Request<never, Blog | void, Blog>, res) => {
+  const dataFromClient: Blog = req.body;
+  const newBlog: Blog = (await Blogs.create(dataFromClient)).toJSON();
+  res.json(newBlog);
+});
+
+router.get(
+  "/:id",
+  async (req: Request<{ id: string }, Blog | string, never>, res) => {
+    const { id } = req.params;
+    const blog = await Blogs.findByPk(Number(id));
+    if (blog) {
+      res.json(blog.toJSON());
+    } else {
+      res.sendStatus(404);
+    }
+  }
+);
+
+router.put(
+  "/:id",
+  async (
+    req: Request<{ id: string }, Blog | string, { likes: number | string }>,
+    res
+  ) => {
+    const { id } = req.params;
+    const { likes } = req.body;
+    const blog = await Blogs.findByPk(id);
+    if (blog) {
+      await blog.update({ likes });
+      res.json(blog.toJSON());
+    } else {
+      res.sendStatus(404);
+    }
+  }
+);
+
+export default router;
